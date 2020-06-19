0c75e681830a89453854cc06a99c567a03037250
aiyubalikhan92 <aiyubalikhan92@gmail.com>
2020-06-20 01:04:23 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -5,8 +5,8 @@ source ./ci/config.sh
 export VERSION=$1
 
 docker build --pull -t "experiment-nginx:latest" -f ./ci/Dockerfile .
-docker tag "experiment-nginx:latest" "769910020948.dkr.ecr.eu-west-1.amazonaws.com/experiment-nginx:$VERSION"
-$(aws ecr get-login --region "eu-west-1" --no-include-email)
-docker push "769910020948.dkr.ecr.eu-west-1.amazonaws.com/experiment-nginx:$VERSION"
+docker tag "experiment-nginx:latest" "208050583360.dkr.ecr.us-east-1.amazonaws.com/experiment-nginx:$VERSION"
+$(aws ecr get-login --region "es-east-1" --no-include-email)
+docker push "208050583360.dkr.ecr.us-east-1.amazonaws.com/nodepaa/experiment-nginx:$VERSION"
 
 echo "Docker image experiment-nginx:$VERSION prepared and pushed"
